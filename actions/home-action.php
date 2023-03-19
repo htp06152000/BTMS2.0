a769891877a3a18b0392e7316a61acef928393ec
htp06152000 <90561047+htp06152000@users.noreply.github.com>
2023-03-19 22:29:56 +0800

    19/3/23

--- actions/home-action.php
+++ actions/home-action.php
@@ -8,13 +8,14 @@
 		$numbr = sanitize_input($_POST['trackingnumber']);
 
 		$result = $DB->prepare("SELECT * FROM transaction WHERE transactionID = ? LIMIT 0, 1");	
-		$result->execute([ $numbr ]);
+		$result->execute([ $_GET[ 'track' ]]);
 		
-		if ($result && $result->rowCount() > 0) {
+		if ($result && $result->rowCount() > 0) 
+			{
 			
 			$_SESSION['message'] 		= "Found!";
 			$_SESSION['messagetype'] 	= "warning";
-			redirect_to('home');
+			redirect_to('login');
 		} else {
 			$_SESSION['message'] 		= "Incorrect credentials!";
 			$_SESSION['messagetype'] 	= "warning";
--- pages/home.php
+++ pages/home.php
@@ -120,7 +120,7 @@
                         <h4 class="mb-3 mb-md-0 pe-1 text-start">Track</h4>
                         <h4 class="mb-3 mb-md-0 text-start" id="request">Request</h4>
 
-                        <form method="post" class="input-group news-input ps-5">
+                        <form method="POST" class="input-group news-input ps-5">
                                 <input name="trackingnumber" type="text" class="form-control" placeholder="Enter code here">
                                 <button class="btn btn-success btn-md text-light" name="track" type="submit">Track</button>
                         </form>
